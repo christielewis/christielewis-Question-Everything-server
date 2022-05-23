820b9a4982b719c23e1d3d46dbabc303c1b33f5b
Christie Lewis <christielewis@Christies-MacBook-Air.local>
2022-05-23 16:20:47 -0400

    edited and ran cURL scripts for change-pw and sign-out; server fully functional

--- curl-scripts/auth/change-password.sh
+++ curl-scripts/auth/change-password.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# sh curl-scripts/auth/change-password.sh  
+
+OLDPW="cc"
+NEWPW="c"
+TOKEN="f9e6f743e942a0c1b09f3c79f70c6784"
+
 API="http://localhost:4741"
 URL_PATH="/change-password"
 
--- curl-scripts/auth/sign-out.sh
+++ curl-scripts/auth/sign-out.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# sh curl-scripts/auth/sign-out.sh 
+
+TOKEN="f9e6f743e942a0c1b09f3c79f70c6784"
+
 API="http://localhost:4741"
 URL_PATH="/sign-out"
 
